da1ec93f6fdc5f07cf6f6cc6da995127df70b026
zienbastian33 <bastian.chacon93@gmail.com>
2023-06-03 05:59:07 -0400

    Se agregaron las imagenes asi como los botones requeridos, dentro de la pagina projects.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "index",     to:"portafolio#index"
   get "home",     to:"portafolio#home"
   get "projects",       to:"portafolio#projects"
   get "contact",       to:"portafolio#contact"
   # Defines the root path route ("/")
   # root "articles#index"
+  root "portafolio#index"
 end
