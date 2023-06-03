a917e81e4725449654edf24c11b76e2ba6d7908f
zienbastian33 <bastian.chacon93@gmail.com>
2023-06-03 04:56:16 -0400

    Se agrego la navbar como main con sus links correspondientes con la ayuda del helpet link to

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "home",     to:"portafolio#home"
   get "projects",       to:"portafolio#projects"
-  get "contact",       to:"portafolios#contact"
+  get "contact",       to:"portafolio#contact"
   # Defines the root path route ("/")
   # root "articles#index"
 end
